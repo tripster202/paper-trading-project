cab4ba91e3bee35528142294ba5c57d45cd21e3e
tripster202 <spookybooleanspoon@yahoo.com>
2024-11-23 13:47:49 -0700

    templated plan on main

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 print("Hello World!")
-print("Echo Echo")
+
+# plan of action
+
+# set base dollar amount of funds $100.00
+
+# three different strategies
+# moving averages (golden cross)
+# MACD
+# RSI
+
+# trade based on open, close, high, low, OHLC-4 (combination)
+# one trade per day
+# buy the entire amount on entry signal
+# sell the entire amount on exit signal
+
+# 7 choices of stocks (mag 7)
+
+# specify time period of analysis (N days)
+# yfinance offers a base set of time periods -- offer the same to the user
+
+# pull data on the given stock over that time period
+
+# apply the technical analysis over the price data
+# buy and sell accordingly
+
+# three seperate outcomes according to the three separate strategies
+# the outcomes will be a mix of cash and stock
+# which outcome performed the best over the given time period?
